5e6c1c47fd4a5a427d2d980f041b94d435762f60
superKalo <superkalo@devlabs.bg>
2016-01-17 14:32:26 +0200

    Added: main structure for the bootstrap theme

---
+++ home.php
@@ -0,0 +1,98 @@
+<?php
+    $title = 'Кабели и Ел. Материали | MG Електроника Варна';
+    $meta_description = 'Търговия с кабели, ел. материали, осветителни тела, електрически лампи и други | MG Електроника Варна';
+    $meta_keywords = 'кабели варна, ел. материали варна, осветителни тела варна, ключове, контакти, съединители, ел. лампи, енергоспестяващи лампи';
+
+    include 'mainElements/head-bootstrap.php';
+?>
+
+<body>
+
+    <nav class="navbar navbar-default">
+        <div class="container">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">
+                    <img src="img/MG_Elektronika_Logo.png" width="186" height="38" alt="Лого на MG Електроника Варна" title="Към началната страница | MG Електроника Варна" itemprop="logo" />
+                </a>
+            </div>
+
+            <p class="navbar-text">Магазин за кабели и ел. материали</p>
+
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="active"><a href="#">За нас</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Продукти <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Кабли и кабелна арматура</a></li>
+                            <li><a href="#">Осветителни тела</a></li>
+                            <li><a href="#">Ключове и контакти</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Връзка с нас</a></li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div>
+    </nav>
+
+    <header class="jumbotron">
+        <img src="/img/MG_elektronika_InsideImg.jpg" class="img-responsive" alt="" />
+
+        <div class="container">
+            <h1>Кабели и ел. материали Варна</h1>
+            <p>Магазин на едро и дребно</p>
+        </div>
+    </header>
+
+    <section class="container">
+        <p>MG Електроника Варна е магазин за ел. материали, който се намира до варненската автогара - на <a href="контакти" title="Виж точното местоположение">бул. "Владислав Варненчик" №138</a>.</p>
+        <p>При нас можете да намерите голямо разнообразие от <strong>ел. материали</strong>, широка гама от най-популярните и използвани <a href="кабели-и-кабелна-арматура" title="Разгледай предлаганите от нас кабели и кабелна арматура"><strong>кабели</strong> и <strong>проводници</strong></a>, кабелни канали и разклонителни кутии.</p>
+        <p>Предлагаме също и различни видове съединители и разклонители. В асортимента на предлаганите от нас продукти се включват акумулаторни батерии и такива за еднократна употреба, разнообразни видове <a href="осветителни-тела" title="Виж предлаганите от нас осветителни тела"><strong>осветителни тела</strong></a>: енергоспестяващи лампи, крушки, луминисцентни пури, халогенни лампи и др.</p>
+        <p>Ако имате конкретни въпроси за инсталации или отделни електрически компоненти, не сте сигурни или просто искате да потърсите съвет - пишете ни на посочените координати - ще направим всичко възможно, за да Ви бъдем полезни и да Ви отговорим в най-кратък срок.</p>
+    </section>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4" itemprop="makesOffer" itemscope itemtype="http://schema.org/Product">
+                <a href="кабели-и-кабелна-арматура" title="Кабели и кабелна арматура | AETON" class="trippleLink" itemprop="url">
+                    <img src="img/Eaton_logoImg.jpg" alt="Устройства за контрол и разпределение на електрическата енергия AETON" itemprop="image"/>
+                    <h2 itemprop="name">Кабели и кабелна арматура</h2>
+                    <h3 itemprop="description">... вижте предлаганите от нас кабели и кабелна арматура: автоматични предпазители с една година гаранция, разклонителни куртии, кабелни канали, стълбищни автомати и др.</h3>
+                </a>
+            </div>
+            <div class="col-md-4" itemprop="makesOffer" itemscope itemtype="http://schema.org/Product">
+                <a href="осветителни-тела" title="Осветителни тела | Ultralux" class="trippleLink" itemprop="url">
+                    <img src="img/Ultralux_logoImg.jpg" alt="Лампи Ultralux" itemprop="image" />
+                    <h2 itemprop="name">Осветителни тела</h2>
+                    <h3>... разгледайте разнообразните видове налични осветителни тела: енерго-спестяващи лампи "Ultralux" с една година гаранция, крушки, луминисцентни пури, халогенни лампи и много др.</h3>
+                </a>
+            </div>
+            <div class="col-md-4" itemprop="makesOffer" itemscope itemtype="http://schema.org/Product">
+                <a href="ключове-и-контакти" title="Ключове и контакти | Gunsan" class="trippleLink" itemprop="url">
+                    <img src="img/Gunsan_logoImg.jpg" alt="Ключове и контакти Gunsan" itemprop="image"/>
+                    <h2 itemprop="name">Ключове и контакти</h2>
+                    <h3>... запознайте се с наличния в магазина асортимент, който включва ключове и контакти "Gunsan" с една година гаранция, щепсели, съединители и разклонители и др.</h3>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="jumbotron container text-center">
+        <p>Имаш въпрос?</p>
+        <a class="btn btn-lg btn-primary" href="#" role="button">Свържи се с нас</a>
+    </div>
+
+
+    <footer class="footer container">
+        <a href="http://mgelektronika.com">MG Електроника</a> &copy; 2012-2016 | Варна, бул. “Владислав Варненчик” №138
+
+        <div class="pull-right">
+            <a href="карта-на-сайта">Карта на сайта</a> |
+            <a href="за-нас">За нас</a> |
+            <a href="продукти">Продукти</a> |
+            <a href="контакти">Контакти</a>
+        </div>
+    </footer>
+
+</body>
+</html>
---
+++ mainElements/head-bootstrap.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="bg">
+<head>
+    <meta charset="utf-8" />
+    <title><?= $title ?></title>
+
+    <meta name="description" content="<?= $meta_description ?>" />
+    <meta name="keywords" content="<?= $meta_keywords ?>" />
+
+    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+    <link rel="author" href="https://plus.google.com/102267809340505390827" />
+
+    <!--[if lt IE 9]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <script type="text/javascript">
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-21569700-7']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+    </script>
+
+</head>
